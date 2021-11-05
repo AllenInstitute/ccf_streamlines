3dbaa8093899e322dbb66600bca080abe42c7c97
Nathan Gouwens <nathang@alleninstitute.org>
2021-11-05 10:50:17 -0700

    Add additional SWC-related functions

--- ccf_streamlines/morphology.py
+++ ccf_streamlines/morphology.py
@@ -4,6 +4,31 @@ from ccf_streamlines.coordinates import coordinates_to_voxels
 
 
 def load_swc_as_dataframe(swc_file):
+    """ Load a morphology SWC file into a pandas DataFrame.
+
+    The dataframe contains the columns:
+        - ID : node identifier
+        - type : node type, which could be
+            1 = soma
+            2 = axon
+            3 = basal dendrite (or generic dendrite)
+            4 = apical dendrite
+        - x : x-coordinate
+        - y : y-coordinate
+        - z : z-coordinate
+        - r : radius
+        - parent_id : identifier of parent node
+
+    Parameters
+    ----------
+    swc_file : str
+        File path of SWC file
+
+    Returns
+    -------
+    df : DataFrame
+        Dataframe with morphology information
+    """
     return pd.read_table(
         swc_file,
         sep=" ",
@@ -24,9 +49,9 @@ def transform_swc_to_volume(
     ----------
     swc_file : str
         File path of SWC file
-    volume_shape : 3-tuple of ints
+    volume_shape : 3-tuple of ints, default (1320, 800, 1140)
         Shape of target volume in voxels
-    resolution : 3-tuple of ints
+    resolution : 3-tuple of ints, default (10, 10, 10)
         Size of voxels in microns
     compartments : list-like, optional
         List of compartment types to include
@@ -51,6 +76,31 @@ def transform_swc_to_volume(
 
     # Determine voxels from coordinates
     coords = swc_df.loc[:, ['x', 'y', 'z']].values
+
+    return transform_coordinates_to_volume(coords, volume_shape, resolution)
+
+
+def transform_coordinates_to_volume(
+    coords,
+    volume_shape=(1320, 800, 1140),
+    resolution=(10, 10, 10),
+    ):
+    """ Create a volume with counts of points.
+
+    Parameters
+    ----------
+    coords : (N, 3) array
+        Coordinates of points
+    volume_shape : 3-tuple of ints, default (1320, 800, 1140)
+        Shape of target volume in voxels
+    resolution : 3-tuple of ints, default (10, 10, 10)
+        Size of voxels in microns
+
+    Returns
+    -------
+    volume : array
+        Volume with shape `volume_shape` with counts of points in each voxel
+    """
     voxels = coordinates_to_voxels(coords, resolution=resolution)
 
     # Count the nodes in each voxel
@@ -61,3 +111,24 @@ def transform_swc_to_volume(
     volume[populated_voxels[:, 0], populated_voxels[:, 1], populated_voxels[:, 2]] = counts
 
     return volume
+
+
+def find_topological_point_coordinates(swc_df):
+    """ Get the coordinates of the branch and termination nodes
+
+    Parameters
+    ----------
+    swc_df : dataframe
+        Dataframe with morphology information
+
+    Returns
+    -------
+    coords : (N, 3) array
+        3-D coordinates of branch and termination nodes
+    """
+
+    child_counts = swc_df["parent_id"].value_counts()
+    branch_ids = child_counts[child_counts > 1].index.tolist()
+    term_ids = [i for i in swc_df["id"] if i not in child_counts.index]
+
+    return swc_df.set_index("id").loc[branch_ids + term_ids, ["x", "y", "z"]].values
