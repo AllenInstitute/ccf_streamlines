6f3ece4c4f0adc73e478ec4598239ce67f5a9435
Nathan Gouwens <nathang@alleninstitute.org>
2022-10-13 12:57:59 -0700

    Make version more consistent across package

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'ccf_streamlines'
 copyright = '2022, Nathan Gouwens'
 author = 'Nathan Gouwens'
-release = '0.1'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
