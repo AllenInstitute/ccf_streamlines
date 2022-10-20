3d37a11941ce20bb98edb044e899dc7db6253032
Nathan Gouwens <nathang@alleninstitute.org>
2022-10-20 16:45:41 -0700

    Add mock imports for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
+autodoc_mock_imports = ["h5py", "numpy", "scipy", "pandas", "nrrd", "tqdm", "skimage"]
 
 
 # -- Options for HTML output -------------------------------------------------
